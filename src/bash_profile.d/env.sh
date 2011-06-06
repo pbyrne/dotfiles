aa9b45558b0756a112ce5a46906e7e78883d95cc
Patrick Byrne <code@patrickbyrne.net>
2011-06-06 13:39:04 -0500

    added a tst's installed mysql bin directory to my path

--- src/bash_profile.d/env.sh
+++ src/bash_profile.d/env.sh
@@ -1,5 +1,5 @@
 # trying brew installations of things rather than macports
 #export PATH=/opt/local/bin:/opt/local/sbin:~/Dropbox/bin:~/.gem/ruby/1.8/bin:$PATH
-export PATH=~/Dropbox/bin:~/.gem/ruby/1.8/bin:/usr/local/bin:/usr/local/sbin:$PATH
+export PATH=~/Dropbox/bin:~/.gem/ruby/1.8/bin:/usr/local/bin:/usr/local/sbin:/usr/local/mysql-5.1.43-osx10.6-x86_64/bin:$PATH
 export SHARED_WORKSPACE=~/workspace
 export EDITOR=~/Dropbox/bin/vim
