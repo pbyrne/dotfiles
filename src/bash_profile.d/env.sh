9fa67e1f76c4a9eaeb574043900c1aeb64029b72
Patrick Byrne <patrick.byrne@tstmedia.com>
2011-06-29 16:43:44 -0500

    added "local" to the default POW domains

--- src/bash_profile.d/env.sh
+++ src/bash_profile.d/env.sh
@@ -3,3 +3,5 @@
 export PATH=~/Dropbox/bin:~/.gem/ruby/1.8/bin:/usr/local/bin:/usr/local/sbin:/usr/local/mysql-5.1.43-osx10.6-x86_64/bin:$PATH
 export SHARED_WORKSPACE=~/workspace
 export EDITOR=~/Dropbox/bin/vim
+export POW_DOMAINS=dev,local
+
