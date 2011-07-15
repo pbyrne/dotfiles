7689f6a92ecfc025f6620d8545206e370745bef3
Patrick Byrne <patrick.byrne@tstmedia.com>
2011-07-15 13:08:58 -0500

    need to load the mysql version of mysql, not homebrew version

--- src/bash_profile.d/env.sh
+++ src/bash_profile.d/env.sh
@@ -1,6 +1,6 @@
 # trying brew installations of things rather than macports
 #export PATH=/opt/local/bin:/opt/local/sbin:~/Dropbox/bin:~/.gem/ruby/1.8/bin:$PATH
-export PATH=~/Dropbox/bin:~/.gem/ruby/1.8/bin:/usr/local/bin:/usr/local/sbin:/usr/local/mysql-5.1.43-osx10.6-x86_64/bin:$PATH
+export PATH=~/Dropbox/bin:~/.gem/ruby/1.8/bin:/usr/local/mysql-5.1.50-osx10.6-x86_64/bin:/usr/local/bin:/usr/local/sbin:$PATH
 export SHARED_WORKSPACE=~/workspace
 export EDITOR=~/Dropbox/bin/vim
 export POW_DOMAINS=dev,local
