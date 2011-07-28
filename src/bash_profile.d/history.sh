a20e9a5eddcc6c03fdbfadf6adffe263ef02279c
Patrick Byrne <patrick.byrne@tstmedia.com>
2011-07-28 08:40:39 -0500

    added HISTSIZE entry, since i dunno what happens when the two entries have different values

--- src/bash_profile.d/history.sh
+++ src/bash_profile.d/history.sh
@@ -1,3 +1,6 @@
+# both of these do the same thing, dunno what happens if they have different values
 export HISTFILESIZE=5000
+export HISTSIZE=5000
+# ignore duplicates in command history (used with up and down arrow to redo command)
 export HISTCONTROL=ignoredups
 alias hgrep='history | grep $1'
