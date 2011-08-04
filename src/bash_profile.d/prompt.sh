136586e73a23025b286bbd2b2ac171342b0c6165
Patrick Byrne <patrick.byrne@tstmedia.com>
2011-08-04 10:45:24 -0500

    Updated prompt
    * Removed username and host, since those I can infer now that I'm not
      SSHing out to my development boxes much
    * Added comments regarding what the pieces mean

--- src/bash_profile.d/prompt.sh
+++ src/bash_profile.d/prompt.sh
@@ -3,5 +3,12 @@ hg_ps1() {
     hg prompt "{branch}{status}" 2> /dev/null
 }
 
-
-export PS1="\u @ \h in \w $BOLD\$(hg_ps1)\$(__git_ps1 2> /dev/null)$NC \n$ "
+# for reference:
+# \u is username
+# \h is hostname
+# \w is pathname
+# __git_ps1 is the git prompt information
+# rvm-prompt is the RVM prompt information
+# $BOLD bolds text, \NC resets back to normal color
+# 2> /dev/null swallows any errors
+export PS1="\w \$(rvm-prompt 2> /dev/null)$BOLD\$(__git_ps1 2> /dev/null)$NC \n$ "
