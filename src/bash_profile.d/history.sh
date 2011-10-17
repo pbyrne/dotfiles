ef1845c92f737aceefb630610507169d7fae1c2d
Patrick Byrne <patrick.byrne@tstmedia.com>
2011-10-17 08:26:39 -0500

    Update history in realtime; Ignore simple commands in history

--- src/bash_profile.d/history.sh
+++ src/bash_profile.d/history.sh
@@ -3,4 +3,10 @@ export HISTFILESIZE=5000
 export HISTSIZE=5000
 # ignore duplicates in command history (used with up and down arrow to redo command)
 export HISTCONTROL=ignoredups
+# ignore simple commands
+export HISTIGNORE="&:ls:[bf]g:exit:%[0-9]"
+
+# append each history entry from ALL terminals in realtime (not after session ending)
+shopt -s histappend
+
 alias hgrep='history | grep $1'
