f5e1d8a01c47635b28e3e5b58e1d88e258e3c088
Patrick Byrne <patrick.byrne@tstmedia.com>
2011-12-05 12:08:25 -0600

    Add work() command to take me to given project
    Also add _work() bash-completion to autocomplete with the directories in
    ~/workspace/.

--- src/bash_profile.d/functions.sh
+++ src/bash_profile.d/functions.sh
@@ -3,3 +3,21 @@ current_git_branch() {
   grep '^\* ' |
   awk '{ print $2 }'
 }
+
+work() {
+  cd ~/workspace/$1
+}
+
+# bash completion function for work()
+_work() {
+  local cur
+  local trim
+  cur=${COMP_WORDS[COMP_CWORD]}
+  # length of expanded path, used to trim off first portion of matched paths below
+  trim=`echo ~/workspace/ | wc -c`
+  # perform completion, returning all directories in workspace, trimming off the path to the workspace
+  COMPREPLY=( $( compgen -S/ -d ~/workspace/$cur | cut -b $trim- ) )
+}
+
+# enable completion for work() function using _work(), but only if `complete` exists
+command -v complete && complete -o nospace -F _work work
