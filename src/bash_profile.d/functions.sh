25bae42b54fa8ed4e7adf285d8845014d36dcff6
Patrick Byrne <patrick.byrne@tstmedia.com>
2011-12-05 12:59:29 -0600

    Update work() and _work() to use $SHARED_WORKSPACE environment variable instead of hard-coded ~/workspace

--- src/bash_profile.d/functions.sh
+++ src/bash_profile.d/functions.sh
@@ -4,19 +4,26 @@ current_git_branch() {
   awk '{ print $2 }'
 }
 
+workspace() {
+  result=$([ -s "$SHARED_WORKSPACE" ] && echo $SHARED_WORKSPACE || echo "~/workspace/")
+  echo "$result"
+}
+
 work() {
-  cd ~/workspace/$1
+  dir=$(workspace)
+  cd "$dir/${1}"
 }
 
 # bash completion function for work()
 _work() {
   local cur
   local trim
+  local dir=$(workspace)
   cur=${COMP_WORDS[COMP_CWORD]}
   # length of expanded path, used to trim off first portion of matched paths below
-  trim=`echo ~/workspace/ | wc -c`
+  trim=`echo $dir/ | wc -c`
   # perform completion, returning all directories in workspace, trimming off the path to the workspace
-  COMPREPLY=( $( compgen -S/ -d ~/workspace/$cur | cut -b $trim- ) )
+  COMPREPLY=( $( compgen -S/ -d $dir/$cur | cut -b $trim- ) )
 }
 
 # enable completion for work() function using _work(), but only if `complete` exists
