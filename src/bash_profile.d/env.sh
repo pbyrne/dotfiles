3e228c3b87b5e6593e0efb0836afa4e0398da6fa
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-01-06 08:19:55 -0600

    Update from MySQL 5.1 to MySQL 5.5

--- src/bash_profile.d/env.sh
+++ src/bash_profile.d/env.sh
@@ -1,6 +1,5 @@
-# trying brew installations of things rather than macports
-#export PATH=/opt/local/bin:/opt/local/sbin:~/Dropbox/bin:~/.gem/ruby/1.8/bin:$PATH
-export PATH=~/Dropbox/bin:~/.gem/ruby/1.8/bin:/usr/local/mysql-5.1.50-osx10.6-x86_64/bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/python:$PATH
+export PATH=~/Dropbox/bin:/usr/local/mysql/bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/python:$PATH
+export DYLD_LIBRARY_PATH=/usr/local/mysql/lib/:$DYLD_LIBRARY_PATH
 export SHARED_WORKSPACE=~/workspace
 export EDITOR=~/Dropbox/bin/vim
 # in LION, needed this to compile ruby 1.8.7
