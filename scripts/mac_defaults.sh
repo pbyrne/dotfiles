e03c8f73f19eb5b3435991357ff77fe3632c88b0
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-05-14 05:54:15 -0500

    Automate adding the "Action" folder to PDF printing

--- scripts/mac_defaults.sh
+++ scripts/mac_defaults.sh
@@ -67,3 +67,7 @@ defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 
 # do not restart the prior open tabs when opening Terminal
 defaults write com.apple.Terminal NSQuitAlwaysKeepsWindows -int 0
+
+# add ~/Dropbox/Action to the list of PDF printing locations
+ln -s ~/Dropbox/Action ~/Library/PDF\ Services/Action
+
