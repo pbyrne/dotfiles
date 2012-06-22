405474fa7eaddb12cc890c3a0ae92fe81b1b38e4
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-06-22 11:06:21 -0500

    blarg, starting but failing to do some bindkeys work

---
+++ src/zsh/bindkeys.zsh
@@ -0,0 +1,7 @@
+# http://www.cs.elte.hu/zsh-manual/zsh_14.html
+# To see the key combo you want to use just do:
+# cat > /dev/null
+# And press it
+
+# bindkey "^S" backward-word
+# bindkey "^D" forward-word
