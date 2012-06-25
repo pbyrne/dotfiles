64da1bdc0cebeac4a4be348ddec8d27161b0e8f8
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-06-25 11:12:10 -0500

    Remove no-longer-used __git_ps1 environment variables

--- src/zsh/environment.zsh
+++ src/zsh/environment.zsh
@@ -8,17 +8,6 @@ export CC=/usr/bin/gcc-4.2
 export NODE_PATH=/usr/local/lib/node_modules
 export GREP_OPTIONS='--color=auto'
 
-# for more info, http://git.kernel.org/?p=git/git.git;a=blob;f=contrib/completion/git-completion.bash;hb=HEAD
-# __git_ps1 flags
-# show * if there are untracked changes and + if staged and uncommitted changes
-GIT_PS1_SHOWDIRTYSTATE=1
-# show $ if there are stashed changes
-GIT_PS1_SHOWSTASHSTATE=1
-# show % if there are untracked files
-GIT_PS1_SHOWUNTRACKEDFILES=1
-# show < if there are unpulled changes, > if there are unpushed changes, or <> if there are both
-GIT_PS1_SHOWUPSTREAM=1
-
 # REE settings to help tests run faster
 # stolen from http://smartic.us/2010/10/27/tune-your-ruby-enterprise-edition-garbage-collection-settings-to-run-tests-faster/
 export RUBY_HEAP_MIN_SLOTS=1000000
