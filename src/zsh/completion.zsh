2069e049b567f67520fc99041bb62cdef35278ed
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-06-26 07:25:08 -0500

    Case-insensitive zsh completion, lifted from oh-my-zsh

--- src/zsh/completion.zsh
+++ src/zsh/completion.zsh
@@ -1,2 +1,10 @@
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path ~/.zsh/cache
+
+## case-insensitive (all),partial-word and then substring completion
+if [ "x$CASE_SENSITIVE" = "xtrue" ]; then
+  zstyle ':completion:*' matcher-list 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+  unset CASE_SENSITIVE
+else
+  zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+fi
