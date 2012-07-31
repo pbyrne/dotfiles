b1de90feb92115999a57fdc69cfece70f7fa821c
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-07-31 13:35:02 -0500

    remove unneeded CC environment variable; things install just fine now without it

--- src/zsh/environment.zsh
+++ src/zsh/environment.zsh
@@ -3,8 +3,6 @@ export DYLD_LIBRARY_PATH=/usr/local/mysql/lib/:$DYLD_LIBRARY_PATH
 export SHARED_WORKSPACE=~/workspace
 # open in gui vim and wait for the window to close
 export EDITOR='vim'
-# in LION, needed this to compile ruby 1.8.7
-export CC=/usr/bin/gcc-4.2
 export NODE_PATH=/usr/local/lib/node_modules
 export GREP_OPTIONS='--color=auto'
 
