0b2c49ff956f84835e4f2c1368815c333f38f00c
Patrick Byrne <patrick.byrne@tstmedia.com>
2013-03-01 13:11:00 -0600

    add alias for "grep -v"

--- src/zsh/aliases.zsh
+++ src/zsh/aliases.zsh
@@ -6,6 +6,7 @@ alias du='du -hc'
 alias psg='ps aux | grep -v grep | grep'
 
 alias -g G='| grep'
+alias -g GV='| grep -v'
 alias -g M='| less'
 alias -g L='| wc -l'
 alias -g ONE="| awk '{ print \$1 }'"
