5a7a767610bf6616ea032c2af4c132b620b3b1a7
Patrick Byrne <patrick.byrne@tstmedia.com>
2013-05-06 16:40:41 -0500

    Tweak the JS Syntax

--- src/js/cloud.engineyard.com.js
+++ src/js/cloud.engineyard.com.js
@@ -1,10 +1,9 @@
-if ($('#app .environment-name').text().indexOf('production') >= 0) {
-  background = {
+if ($('#app .environment-name').text().match('production')) {
+  $('#content').css({
     'background-color': "rgba(255, 0, 0, 0.55)",
     'background-image': "url('http://i.imgur.com/i8KY7.gif')",
     'background-position': 'left top',
     'background-repeat': 'repeat'
-  }
-  $('#content').css(background)
+  })
   $('.page').css('background-color', 'white')
 }
