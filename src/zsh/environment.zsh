1daf3a117a2c88029dcfd2bbf7d9cf6d4ec47f3f
Patrick Byrne <patrick.byrne@tstmedia.com>
2013-07-09 09:50:17 -0500

    Bye-Bye, MySQL

--- src/zsh/environment.zsh
+++ src/zsh/environment.zsh
@@ -1,5 +1,4 @@
 # export PATH=~/Dropbox/bin:/usr/local/mysql/bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/python:$PATH
-export DYLD_LIBRARY_PATH=/usr/local/mysql/lib/:$DYLD_LIBRARY_PATH
 export SHARED_WORKSPACE=~/workspace
 # open in gui vim and wait for the window to close
 export EDITOR='vim'
