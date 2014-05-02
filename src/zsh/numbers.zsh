8f7270d36466d7dac4b4daaee879971ca62079a2
Patrick Byrne <code@patrickbyrne.net>
2014-05-02 08:45:56 -0500

    New number function to sum up STDIN

--- src/zsh/numbers.zsh
+++ src/zsh/numbers.zsh
@@ -5,3 +5,11 @@
 function mean() {
  awk '{ total += $1; count++ } END { print total/count }'
 }
+
+# calculate the sum of the numbers passed in, assumes one number per line
+#
+# usage: pbpaste | total
+# usage: total < file.txt
+function total() {
+  awk '{ total += $1 } END { print total }'
+}
