7487e35e3cbab88093d1fbae2a4bef534800f5f6
Patrick Byrne <code@patrickbyrne.net>
2015-08-06 16:07:07 -0500

    Fix exception in command-frequency function

--- src/zsh/command-frequency.zsh
+++ src/zsh/command-frequency.zsh
@@ -1,9 +1,7 @@
 # List commands in history (grouped by the first two 'words', e.g., "git
-# commit" or "cd ~") and orderd from most-used to least-used.
+# commit" or "cd ~") and ordered from most-used to least-used.
 function command-frequency() {
-  # LC_CTYPE=C LANG=C causes cut to not fall down on control characters ending up in .history
-  # http://stackoverflow.com/questions/19242275/re-error-illegal-byte-sequence-on-mac-os-x
-  LC_CTYPE=C LANG=C cut -d ';' -f 2 < ~/.history 2> /dev/null | awk '{print $1 " " $2}' | sort | uniq -c | sort -rn
+  cut -d ';' -f 2 < ~/.history 2> /dev/null | awk '{print $1 " " $2}' | sort | uniq -c | sort -rn
 }
 
 # List the most-frequently used commands
