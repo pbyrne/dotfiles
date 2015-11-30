658bb47079f072efe8e7dffe6d9d51d78d28f7a3
Patrick Byrne <code@patrickbyrne.net>
2015-11-30 11:29:13 -0600

    Set up emacs with evil mode enabled

---
+++ src/emacs.d/elpa/goto-last-change-20150109.1023/goto-last-change-pkg.el
@@ -0,0 +1,1 @@
+(define-package "goto-last-change" "20150109.1023" "Move point through buffer-undo-list positions" 'nil :url "https://github.com/camdez/goto-last-change.el" :keywords '("convenience"))
