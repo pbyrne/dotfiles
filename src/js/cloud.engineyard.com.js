c86eeea0356c42769a9d748838ff1f94bf116bb4
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2019-04-19 10:39:03 -0500

    Remove unused dotjs user scripts

--- src/js/cloud.engineyard.com.js
+++
@@ -1,9 +0,0 @@
-if ($('#app .environment-name').text().match('production')) {
-  $('#content').css({
-    'background-color': "rgba(255, 0, 0, 0.55)",
-    'background-image': "url('http://i.imgur.com/i8KY7.gif')",
-    'background-position': 'left top',
-    'background-repeat': 'repeat'
-  })
-  $('.page').css('background-color', 'white')
-}
