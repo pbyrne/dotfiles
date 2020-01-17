fdb58165db116dcad035b5db2cf2f9ff9f2ce500
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2020-01-17 09:17:25 -0600

    Whitespace cleanup

--- src/zsh/completion.zsh
+++ src/zsh/completion.zsh
@@ -1,4 +1,3 @@
-
 zstyle ':completion:*' use-cache on
 
 __git_files () {
@@ -12,4 +11,3 @@ if [ "x$CASE_SENSITIVE" = "xtrue" ]; then
 else
   zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 fi
-
